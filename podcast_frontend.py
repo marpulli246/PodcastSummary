9fb9506ba8036f62aa08b5ccc353eb1439287b2b
Markku Pulli <75437735+marpulli246@users.noreply.github.com>
2023-08-20 13:35:49 -0400

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -162,7 +162,7 @@ def main():
     # Footer
     st.markdown(
         """
-        <div style="position: fixed; bottom: 0; left: 0; right: 0; background-color: RoyalBlue; padding: 10px;">
+        <div style="position: fixed; bottom: 0; left: 0; right: 0; background-color: SteelBlue; padding: 10px;">
             <div style="text-align: center;">
                 <a href="https://twitter.com" target="_blank" style="margin-right: 15px;">🐦 Twitter</a>
                 <a href="https://facebook.com" target="_blank" style="margin-right: 15px;">📘 Facebook</a>
