d9a1796022e72b534932a0626e5d604328f9673f
Markku Pulli <75437735+marpulli246@users.noreply.github.com>
2023-08-19 22:14:18 -0400

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -120,7 +120,7 @@ def main():
     <button class="btn-custom" id="processButton">Process Podcast Feed</button>
     """
     
-    st.markdown(process_button, unsafe_allow_html=True)
+    st.sidebar.markdown(process_button, unsafe_allow_html=True)
     #New style button ends
     
     st.sidebar.markdown("""
